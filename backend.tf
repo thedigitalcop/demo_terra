2bba6ea4951147d1df0e441f34910ef0368d4f2d
rajni rajput <samjmu85@gmail.com>
2026-07-09 13:49:50 +0530

    second commit

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "newterra-resource"
-    storage_account_name = "newterrastorage"
-    container_name       = "prod-terra"
-    key                  = "terraform.tfstate"
-  }
-}
