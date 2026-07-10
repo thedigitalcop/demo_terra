d39f1757e3f4a0c46760820b168b564371422b77
rajni rajput <samjmu85@gmail.com>
2026-07-10 10:07:57 +0530

    firfth commit

--- providers.tf
+++ providers.tf
@@ -8,9 +8,5 @@ terraform {
   required_version = ">= 1.14.0"
 }
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
   features {}
 }
