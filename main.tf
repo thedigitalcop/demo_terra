c02d1c3339b9669d5463a60a20926944e31f5c5b
rajni rajput <samjmu85@gmail.com>
2026-07-10 10:11:00 +0530

    sixth commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resource"
+  name     = "newterra-resource"
   location = "West Europe"
   lifecycle {
     prevent_destroy = true
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "example" {
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "${var.prefix}storage"
+  name                     = "newterrastorage"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
